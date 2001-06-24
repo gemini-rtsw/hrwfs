430b03fde534582b6177179f07a14544b4a0dc71
gemvx <gemvx@gemini.edu>
2001-06-24 18:43:01 +0000

    Add standard CAD routine and extend CAR combination to include more CAR records

--- src/hrwfsConfig.c
+++ src/hrwfsConfig.c
@@ -1,18 +1,22 @@
-static char rcsid[]="$Id: hrwfsConfig.c,v 1.1 2001-06-11 08:11:00 cjm Exp $";
+static char rcsid[]="$Id: hrwfsConfig.c,v 1.2 2001-06-24 18:43:01 gemvx Exp $";
 /*
 *   FILENAME
 *   hrwfsConfig.c
 *
 *   FUNCTION NAME(S)
-*   hrwfsCarCombine  - combine configC and activeC CAR records
+*   hrwfsCarCombine  - combine configC, agCommSentC and activeC CAR records
 *   hrwfsConfigBegin - routine to execute at start of configuration checking
 *   hrwfsConfigEnd   - routine to execute at end of configuration checking
 *   hrwfsInterlocked - check if HRWFS is interlocked
+*   hrwfsMechCad     - simple CAD routine for passing parameters to A&G
 *
 */
 /* *INDENT-OFF* */
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2001/06/11 08:11:00  cjm
+ * Source code for hrwfs configuration checking
+ *
  */
 /* *INDENT-ON* */
 
@@ -22,6 +26,7 @@ static char rcsid[]="$Id: hrwfsConfig.c,v 1.1 2001-06-11 08:11:00 cjm Exp $";
 #include <cad.h>
 #include <car.h>
 #include <genSubRecord.h>
+#include <cadRecord.h>
 
 static int hrwfsInterlocked (char *message) ;
 
@@ -30,7 +35,7 @@ static int hrwfsInterlocked (char *message) ;
  *   hrwfsCarCombine
  *
  *   Purpose:
- *   Combines the configC and activeC CAR records to generate applyC
+ *   Combines the configC, agCommSentC and activeC CARs to generate applyC
  *
  *   Description:
  *   Simply pull in the values and messages (if any) and then set applyC
@@ -50,8 +55,12 @@ static int hrwfsInterlocked (char *message) ;
  *   Epics inputs:
  *   a => CAR value of activeC
  *   b => Message field of activeC
- *   c => CAR value of configC
- *   d => Message field of configC
+ *   c => CAR value of activeC in A&G
+ *   d => Message field of activeC in A&G
+ *   e => CAR value of agCommSentC
+ *   f => Message field of agCommSentC
+ *   g => CAR value of configC
+ *   h => Message field of configC
  *
  *   Epics outputs:
  *   vala => Input message for applyC
@@ -63,16 +72,35 @@ static int hrwfsInterlocked (char *message) ;
 long hrwfsCarCombine(struct genSubRecord *pgsub)
 {
   long outval ;
+  char mess[MAX_STRING_SIZE] ;
 
 /* Make output IDLE by default */
   outval = CAR_IDLE ;
+  strcpy(mess, "" ) ;
 
 /* Check for BUSY first */
-  if ( *(long *)pgsub->a == CAR_BUSY || *(long *)pgsub->c == CAR_BUSY) {
+  if ( *(long *)pgsub->a == CAR_BUSY ||
+       *(long *)pgsub->c == CAR_BUSY ||
+       *(long *)pgsub->e == CAR_BUSY ||
+       *(long *)pgsub->g == CAR_BUSY ) {
     outval = CAR_BUSY ;
   } else if (*(long *)pgsub->a == CAR_ERROR ||
-             *(long *)pgsub->c == CAR_ERROR ) {
+             *(long *)pgsub->c == CAR_ERROR ||
+             *(long *)pgsub->e == CAR_ERROR ||
+             *(long *)pgsub->g == CAR_ERROR) {
     outval = CAR_ERROR ;
+
+/* Copy the error message to the output. If there is more than one error
+*  message then the last will get output
+*/
+    if (*(long *)pgsub->a == CAR_ERROR)  
+      strcpy(pgsub->vala, pgsub->b) ;
+    if (*(long *)pgsub->c == CAR_ERROR)  
+      strcpy(pgsub->vala, pgsub->d) ;
+    if (*(long *)pgsub->e == CAR_ERROR)  
+      strcpy(pgsub->vala, pgsub->f) ;
+    if (*(long *)pgsub->g == CAR_ERROR)  
+      strcpy(pgsub->vala, pgsub->h) ;
   }
 
   *(long *)pgsub->valb = outval ;
@@ -223,6 +251,85 @@ int hrwfsInterlocked (char *message)
   strcpy (message, " ") ;
   return 0 ;
 
+
+}
+
+/*+
+ *   Function name:
+ *   hrwfsMechCad
+ *
+ *   Purpose:
+ *   Implements CAD command from HRWFS to A&G 
+ *
+ *   Description:
+ *    General puprose subroutine to implement an A&G HRWFS CAD being
+ *    triggered from a HRWFS CAD. The input strings are simply copied
+ *    to the output strings and a MARK & START directive is output to the
+ *    subsystem CAD via STLK. This routine has been copied with minor 
+ *    modifications from the TCS.
+ *
+ *   Invocation:
+ *    hrwfsMechCad (pcad)
+ *
+ *   Parameters: (">" input, "!" modified, "<" output)
+ *      (!)    pcad     (struct cadRecord *) Pointer to CAD record structure
+ *
+ *   Function value:
+ *   (<)  status  (long) Return status, 0 = OK
+ *
+ *   Prior requirements:
+ *   None
+ *
+ *-
+ */
+
+long hrwfsMechCad (struct cadRecord *pcad)
+{
+   int status;
+
+/* Check for any interlocks */
+
+  if (pcad->dir != CAD_CLEAR)
+    if (hrwfsInterlocked (pcad->mess)) return CAD_REJECT;
+
+  status = CAD_REJECT ;
+
+  switch (pcad->dir)
+  {
+
+   case CAD_PRESET :
+
+   status = CAD_ACCEPT;
+   break ;
+
+   case CAD_START :
+
+/* Copy over the parameters for the A&G CAD */
+
+    strncpy(pcad->vala, pcad->a, MAX_STRING_SIZE) ;
+    strncpy(pcad->valb, pcad->b, MAX_STRING_SIZE) ;
+    strncpy(pcad->valc, pcad->c, MAX_STRING_SIZE) ;
+    strncpy(pcad->vald, pcad->d, MAX_STRING_SIZE) ;
+    strncpy(pcad->vale, pcad->e, MAX_STRING_SIZE) ;
+    strncpy(pcad->valf, pcad->f, MAX_STRING_SIZE) ;
+    status = CAD_ACCEPT;
+   break ;
+
+   case CAD_MARK :
+   status = CAD_ACCEPT;
+   break ;
+
+   case CAD_STOP :
+   status = CAD_ACCEPT;
+   break ;
+
+   case CAD_CLEAR :
+   status = CAD_ACCEPT;
+   break ;
+
+  }
+
+  return(status) ;
 
 }
 
